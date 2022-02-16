7aec0036ae8e0cd73a64c8be89b00c747f0bd8f4
samuel <inboxsamuel@outlook.com>
2022-02-16 11:13:45 +0300

    Added Guiding comments

--- Simple Function/main.go
+++ Simple Function/main.go
@@ -6,6 +6,7 @@ func main() {
 	fmt.Println(add(1, 2))
 }
 
+// function format is func _name(param1 param_type, param1 param_type) type_of_value_to_be_returned{}
 func add(a, b int) int {
 	return a + b
 }
