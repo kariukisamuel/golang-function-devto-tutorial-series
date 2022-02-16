e8488c311690e33e8790c60e28689b60cf54ebdb
samuel <inboxsamuel@outlook.com>
2022-02-16 13:12:00 +0300

    Variables as Functions

--- anonymous-funcs/main.go
+++ anonymous-funcs/main.go
@@ -7,4 +7,11 @@ func main() {
 	func() {
 		fmt.Println("I am an anonymous function")
 	}()
+	//variables as function
+	a := func(name string) string {
+		return name
+	}
+	b := a("sam")
+	println(b)
+
 }
